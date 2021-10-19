2a7e0a871f140c10ddd7b7f51a5b08b80bebf861
sollybabs <87852381+sollybabs@users.noreply.github.com>
2021-10-19 15:28:09 +0200

    update

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 //////////////////// Hamburger Menu
 
-const mobileBtn = document.getElementsById("mobile-cta");
-       nav=document.querySelector('nav');
-       mobileBtnExit=document.getElementById('mobile-exit');
-mobileBtn.addEventListener("click", () => {
-    nav.classList.add('menu-btn');
-});
 
-mobileBtnExit.addEventListener("click", () => {
+const mobileBtn = document.getElementsById("mobile-cta")
+        nav=document.querySelector('nav');
+     mobileBtnExit=document.getElementById('mobile-exit');
+ mobileBtn.addEventListener("click", () => {
+    nav.classList.add('menu-btn');
+ });
+
+ mobileBtnExit.addEventListener("click", () => {
   nav.classList.remove("menu-btn-exit");
-});
+ });
 
 
